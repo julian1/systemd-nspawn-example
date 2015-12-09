94166e509f6bd2aa890b03789e474db98ffe6837
julian1 <julian.atkinson71@gmail.com>
2015-12-10 10:36:24 +1100

    update for jessie

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
-NAME=wily
-VERSION=wily
-MIRROR=http://mirror.aarnet.edu.au/ubuntu/
+NAME=jessie1
+VERSION=jessie
+MIRROR=http://mirror.aarnet.edu.au/debian/
 MAC='00:01:04:1b:2C:1A'
 KEYFILE=/home/meteo/.ssh/id_rsa.pub
 
@@ -12,11 +12,20 @@ if [ ! -d "$NAME" ]; then
   /usr/sbin/debootstrap --verbose $VERSION ./$NAME $MIRROR || exit
 fi
 
-# ssh and python2.7 to support ansible
+# upgrade
 systemd-nspawn -D./$NAME apt-get -y update
+systemd-nspawn -D./$NAME apt-get -y upgrade
+
+# ssh and python2.7 to support ansible
 systemd-nspawn -D./$NAME apt-get -y install ssh python2.7
 systemd-nspawn -D./$NAME ln -s /usr/bin/python2.7 /usr/bin/python
 
+# for debugging
+# systemd-nspawn -D ./$NAME /bin/sh -c 'echo root:root | chpasswd'
+# systemd-nspawn -D./$NAME apt-get -y install tcpdump dhcpdump screen aptitude arping
+
+# allow dhcp to set hostname
+systemd-nspawn -D./$NAME echo > ./$NAME/etc/hostname
 
 ## on host generate keypair - eg.  ssh-keygen -t rsa
 if [ ! -f "$KEYFILE" ]; then
@@ -41,4 +50,10 @@ iface host0 inet dhcp
 hwaddress ether $MAC
 EOM
 
+
+# make ubuntu honor dhcP
+# http://askubuntu.com/questions/104918/how-to-get-the-hostname-from-a-dhcp-server
+# http://blog.schlomo.schapiro.org/2013/11/setting-hostname-from-dhcp-in-debian.html
+# echo unset old_host_name > ./$NAME/etc/dhcp/dhclient-enter-hooks.d/unset_old_hostname
+# ECHO > ./$NAME/etc/hostname
 
--- old/flush.sh
+++ old/flush.sh
@@ -12,15 +12,3 @@ iptables -P FORWARD ACCEPT
 iptables -P OUTPUT ACCEPT
 
 
-# route outgoing
-# iptables  -t nat -A POSTROUTING   -s 10.1.1.0/24   -j MASQUERADE
-iptables  -t nat -A POSTROUTING   -s 10.10.1.0/24   -j MASQUERADE
-
-
-# route to container
-#iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 10.1.1.18 
-#iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j DNAT --to-destination 10.1.1.18 
-#iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j DNAT --to-destination 10.1.1.18 
-
-
-
