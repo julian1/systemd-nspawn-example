2e6f10a0c8081271abe948af67b4af623ba2f7ef
julian1 <julian.atkinson71@gmail.com>
2016-01-08 14:28:57 +1100

    reset script

---
+++ reset.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+NAME=$1
+
+
+# sudo systemctl list-units -a | grep $NAME
+
+sudo systemctl stop machine-$NAME.scope
+sudo systemctl reset-failed machine-$NAME.scope
+
+sudo /sbin/ifconfig vb-$NAME down
+
+# sudo machinectl list -a | grep $NAME
+sudo machinectl kill $NAME
+
+sudo systemctl disable machine-$NAME.scope
+
+
