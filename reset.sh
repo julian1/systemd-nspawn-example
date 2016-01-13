c1322f0c94a944402b0c245e7f7a077370310623
julian1 <julian.atkinson71@gmail.com>
2016-01-13 11:38:19 +1100

    leave user to sudo

--- reset.sh
+++ reset.sh
@@ -2,17 +2,17 @@
 NAME=$1
 
 
-# sudo systemctl list-units -a | grep $NAME
+# systemctl list-units -a | grep $NAME
 
-sudo systemctl stop machine-$NAME.scope
-sudo systemctl reset-failed machine-$NAME.scope
+systemctl stop machine-$NAME.scope
+systemctl reset-failed machine-$NAME.scope
 
-sudo /sbin/ifconfig vb-$NAME down
-sudo /sbin/ifdown vb-$NAME
+/sbin/ifconfig vb-$NAME down
+/sbin/ifdown vb-$NAME
 
-# sudo machinectl list -a | grep $NAME
-# sudo machinectl kill $NAME
+# machinectl list -a | grep $NAME
+# machinectl kill $NAME
 
-# sudo systemctl disable machine-$NAME.scope
+# systemctl disable machine-$NAME.scope
 
 
