c9cafbb609eb7525278514b6a72fe2b9a7cfb69a
julian1 <julian.atkinson71@gmail.com>
2016-04-21 09:33:50 +1000

    better termination of bridge interface

--- reset.sh
+++ reset.sh
@@ -9,8 +9,11 @@ systemctl reset-failed machine-$NAME.scope
 
 brctl delif br0  vb-$NAME
 
-/sbin/ifconfig vb-$NAME down
-/sbin/ifdown vb-$NAME
+# seems to be more robust than ifconfig down, of ifdown
+ip link delete vb-$NAME
+
+#/sbin/ifconfig vb-$NAME down
+#/sbin/ifdown vb-$NAME
 
 # machinectl list -a | grep $NAME
 # machinectl kill $NAME
